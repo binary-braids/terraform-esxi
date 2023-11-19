3d1c07f8a40adb46245d22cfd7884b27b4936d2c
data-tangles <cuanleo@gmail.com>
2023-11-19 16:49:12 +0200

    commit to new branch

---
+++ production/host_02/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags = {
+    environment = var.tag_environment
+    purpose     = var.tag_purpose
+    createdby   = "Terraform"
+    createdon   = formatdate("DD-MM-YYYY hh:mm ZZZ", timestamp())
+    modifiedon  = formatdate("DD-MM-YYYY hh:mm ZZZ", timestamp())
+  }
+}
---
+++ production/host_02/variables.tf
@@ -0,0 +1,60 @@
+## ESXI Host Variables
+
+variable "esxi_hostname" {
+    type = string
+    description = "hostname for ESXI host"
+}
+
+variable "esxi_hostport" {
+    type = string
+    description = "SSH port for ESXI host"
+}
+
+variable "esxi_hostssl" {
+    type = string
+    description = "SSL port for ESXI host"
+}
+
+variable "esxi_username" {
+    type = string
+    description = "username for ESXI host"
+}
+
+variable "esxi_password" {
+    type = string
+    description = "password for ESXI host"
+}
+
+## ESXI Guest Variables 
+
+variable "guest_name" {
+    type = string
+    description = "name for guest VM"
+}
+
+variable "disk_store" {
+    type = string
+    description = "Data Store for guest VM"
+}
+
+variable "clone_from_vm" {
+    type = string
+    description = "Template to clone for new guest VM"
+}
+
+variable "boot_firmware" {
+    type = string
+    description = "Boot firmware for Guest OS"
+}
+
+variable "virtual_network" {
+    type = string
+    description = "Virtual Network for guest VM"
+}
+
+variable "nic_type" {
+    type = string
+    description = "NIC type for guest VM"
+}
+
+
