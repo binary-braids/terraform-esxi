30f09789887cfe463b7c923fcb508d7b0526d75d
data-tangles <cuanleo@gmail.com>
2023-12-03 16:07:28 +0200

    rename vswitch and portgroups

--- production/host_01/modules/networking/main.tf
+++ production/host_01/modules/networking/main.tf
@@ -2,7 +2,7 @@ resource "esxi_vswitch" "production" {
   name = var.vswitch_name
 }
 
-resource "esxi_portgroup" "name" {
+resource "esxi_portgroup" "production" {
   name    = var.port_group_name
   vswitch = esxi_vswitch.production.name
   vlan    = var.port_group_vlan
